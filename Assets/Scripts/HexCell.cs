293cce2a5e83a7d856c2f91f32dec74092eb8a73
Claudio Seitz <zxseitz@gmail.com>
2018-04-06 17:09:11 +0200

    Fixed hex grid

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 public class HexCell : MonoBehaviour {
-    public const float outerRadius = 10f;
+    public const float outerRadius = 1;
     public const float innerRadius = outerRadius * 0.866025404f;
     public static Vector3[] corners = {
         new Vector3(0f, 0f, outerRadius),
@@ -12,6 +12,8 @@ public class HexCell : MonoBehaviour {
         new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
         new Vector3(0f, 0f, outerRadius)
     };
+
+
 
     // Use this for initialization
     void Start () {
