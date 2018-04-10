6719e1b70f999980efe949f966e7d94130cf5122
sschw <sandro.schwager@students.fhnw.ch>
2018-04-11 00:33:00 +0200

    Finished SpriteChooser.

--- Assets/Scripts/SpriteChooser.cs
+++ Assets/Scripts/SpriteChooser.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SpriteChooser : MonoBehaviour {
-	public string emblemPath = "path/to/spritefolder";
+	public string spritePath = "path/to/spritefolder";
 	public Sprite selected;
 
-	private Sprite[] availableSprites;
+
+	Sprite[] availableSprites;
 	private int selectedIndex;
 
 	// Use this for initialization
 	void Start () {
-		object[] loadedFiles = Resources.LoadAll (emblemPath, typeof(Sprite));
+		object[] loadedFiles = Resources.LoadAll (spritePath, typeof(Sprite));
 		availableSprites = new Sprite[loadedFiles.Length+1];
 		for (int i = 0; i < loadedFiles.Length; i++)
 			availableSprites [i+1] = (Sprite)loadedFiles [i];
@@ -23,24 +24,26 @@ public class SpriteChooser : MonoBehaviour {
 		}
 		// Maybe sprite is not in folder. Not allowed so reset.
 		selected = availableSprites [selectedIndex];
-		GetComponentInChildren<Image>().sprite = selected;
+		GetComponentsInChildren<Image>()[1].sprite = selected;
+		GetComponentsInChildren<Button> () [0].onClick.AddListener (() => OnBackClick());
+		GetComponentsInChildren<Button> () [1].onClick.AddListener (() => OnNextClick());
 	}
 	
 	public void OnNextClick() {
-		if (selectedIndex == availableSprites.Length - 1)
+		if (selectedIndex >= availableSprites.Length - 1)
 			selectedIndex = 0;
 		else
 			selectedIndex++;
 		selected = availableSprites [selectedIndex];
-		GetComponentInChildren<Image>().sprite = selected;
+		GetComponentsInChildren<Image>()[1].sprite = selected;
 	}
 
 	public void OnBackClick() {
-		if (selectedIndex == 0)
+		if (selectedIndex <= 0)
 			selectedIndex = availableSprites.Length - 1;
 		else
 			selectedIndex--;
 		selected = availableSprites [selectedIndex];
-		GetComponentInChildren<Image>().sprite = selected;
+		GetComponentsInChildren<Image>()[1].sprite = selected;
 	}
 }
