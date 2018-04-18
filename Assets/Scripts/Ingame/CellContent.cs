b5df154dcd6b78365b0c657ef083ddd0588ca930
Claudio Seitz <zxseitz@gmail.com>
2018-04-18 22:13:16 +0200

    Implemented cell content

---
+++ Assets/Scripts/Ingame/CellContent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public struct CellContent
+{
+    public readonly Sprite Sprite;
+    public readonly CellContent[] Modifikations;
+
+    public CellContent(Sprite sprite, CellContent[] modifikations = null)
+    {
+        Sprite = sprite;
+        Modifikations = modifikations ?? new CellContent[0];
+    }
+}
