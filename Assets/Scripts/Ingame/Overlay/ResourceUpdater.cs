08f0066f75ac3321fdfc580eafe0ee97cd5dbb3a
sschw <sandro.schwager@students.fhnw.ch>
2018-05-22 08:51:58 +0200

    Added Ingame Overlay.

---
+++ Assets/Scripts/Ingame/Overlay/ResourceUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AssemblyCSharp
+{
+	public class ResourceUpdater : MonoBehaviour
+	{
+		public ResourceHandler resources;
+
+		public Text corn;
+		public Text beer;
+
+		public void Update() {
+			corn.text = resources.Corn.ToString ();
+			beer.text = resources.Beer.ToString ();
+		}
+	}
+}
+
